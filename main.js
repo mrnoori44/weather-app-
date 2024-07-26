a3ae161981e5f8d287fc5bdc1a7f828d8ebe6699
mrnoori44 <mohamadnr2002@gmail.com>
2024-07-26 23:04:13 +0330

    final update
    fetching api from the openweathermap

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
+// selecting nessecary classes and store them into variables
 const wrapper = document.querySelector('.wrapper'),
 inputPart = wrapper.querySelector('.input-sec'),
 infoTxt = inputPart.querySelector('.info-txt'),
 inputField = inputPart.querySelector('input'),
-locationBtn = inputPart.querySelector('button');
+locationBtn = inputPart.querySelector('button'),
+weatherIcon = document.querySelector('.weather-part img'),
+arrowBack = wrapper.querySelector('header i');
 let api;
 
-
+// calling request api when enter is pressed
 inputField.addEventListener('keyup', e => {
     if(e.key == "Enter" && inputField.value != ""){
         requestApi(inputField.value);
     }
 });
 
+// if the user browser supports geolocation then onSucces will be called else onError
 locationBtn.addEventListener('click',() => {
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(onSuccess, onError)
@@ -32,12 +36,14 @@ function onError(error){
 }
 
 
+// api url 
 function requestApi(city){
     api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=c1aaf060377e63686336e9657dab62bb`
     fetchData()
     
 }
 
+// fetching info from api
 function fetchData(){
     infoTxt.innerText = "Getting weather details...";
     infoTxt.classList.add('pending');
@@ -48,6 +54,8 @@ function fetchData(){
     .then(result => weatherDetails(result));
 
 }
+
+// showing weather information 
 function weatherDetails(info){
     if(info.cod == '404'){
         infoTxt.classList.replace('pending','error');
@@ -57,6 +65,18 @@ function weatherDetails(info){
         const country = info.sys.country;
         const {description, id} = info.weather[0];
         const {feels_like, humidity, temp} = info.main;
+
+        if(id == 800){
+            weatherIcon.src = 'icons/day.svg';
+        }else if(id >= 200 && id <= 232){
+            weatherIcon.src = 'icons/thunder.svg';
+        }else if(id >= 500 && id <= 531){
+            weatherIcon.src = 'icons/rainy-1.svg';
+        }else if(id >=600 && id <= 622){
+            weatherIcon.src = 'icons/snowy-1.svg';
+        }else if(id >=801 && id <= 804){
+            weatherIcon.src = 'icons/cloudy.svg';
+        }
 
         wrapper.querySelector('.temp .numb').innerText = Math.floor(temp);
         wrapper.querySelector('.weather').innerText = description;
@@ -70,3 +90,7 @@ function weatherDetails(info){
     }
 }
 
+arrowBack.addEventListener('click', () => {
+    wrapper.classList.remove("active");
+    inputField.value = ""
+})
